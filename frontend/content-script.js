9eadc2c360b797fa94fe725b8b07c9d3376dd7ea
Pranav Gor <gor2@illinois.edu>
2024-05-05 21:05:48 -0500

    Finished frontend

---
+++ frontend/content-script.js
@@ -0,0 +1,48 @@
+let create_box = (box, url, content)=>{
+    let content_box = document.createElement("div");
+    content_box.className="content-box";
+    content_box.appendChild(document.createElement("p"));
+    content_box.firstChild.innerHTML = content;
+    content_box.style.width = "100%";
+    let wrapper = document.createElement("div");
+    wrapper.className = "wrapper";
+    wrapper.appendChild(content_box);
+    box.appendChild(wrapper);
+};
+
+chrome.runtime.onMessage.addListener(result =>{
+    console.log(result.action);
+
+    if(result.action == "Summary Ready"){   
+        let box = document.getElementById("Summary-Box");
+        box.removeChild(document.getElementById("Load-text"));
+        create_box(box, result.video_url, result.summary);
+    }
+
+    if(result.action == "Loading Summary"){
+        
+        let player = document.getElementById('top-row');
+        let txt = document.getElementById('Summary-Box');
+        if(txt != null)
+            txt.remove();
+        txt = document.createElement("div");
+        txt.style.margin = "auto";
+        txt.style.border = "5px solid lightcoral";
+        player.parentNode.insertBefore(txt, player.nextSibling);
+        while(txt.childNodes.length > 0)
+            txt.removeChild(txt.firstChild());
+        txt.className = 'style-scope ytd-watch-metadata summary_box';
+        txt.id = "Summary-Box";
+        let temp = document.createElement("p");
+        temp.id = "Load-text";
+        temp.innerHTML = "Loading Summary.......";
+        txt.appendChild(temp);
+        console.log({player, txt});
+    }    
+    
+    if(result.action == "Initalize"){
+        let txt = document.getElementById('Summary-Box');
+        if(txt != null)
+            txt.remove();
+    }
+});
--- frontend/popup.js
+++ frontend/popup.js
@@ -1,1 +1,41 @@
-console.log("This is a popup!")
+summarize = document.getElementById('Summarize');
+const BACKEND_API_URL = 'http://127.0.0.1:8000/summary';
+
+if(summarize){
+    function summarizeVideo(url) {
+        // Make a GET request to the backend API with the video URL as a parameter
+        fetch(`${BACKEND_API_URL}?video_link=${url}`)
+            .then(response => response.json())
+            .then(summary => {
+                console.log(summary);
+                // Send a message to the content script with the summary
+                
+                chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
+                    console.log(tabs);
+                    // chrome.tabs.sendMessage(tabs[0].id, { action: "Summary Ready", summary });
+                    chrome.tabs.sendMessage(tabs[0].id, {"video_url": url, action:"Summary Ready", summary: summary.text_summary}); 
+                    // chrome.runtime.sendMessage({ action: "Summary Ready", summary });
+                });
+            })
+            .catch(error => {
+                console.error('Error:', error);
+            });
+    }
+
+    // Call the summarizeVideo function when the SummaryButton is clicked
+    summarize.onclick = e => {
+
+        chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
+            let url = tabs[0].url.split("&")[0];
+            var expression = /https?:\/\/(www\.)?youtube.com\/watch\b([v?=].*)/g
+            var regex = new RegExp(expression);
+
+            if (!url.match(regex))
+                return;
+
+            chrome.tabs.sendMessage(tabs[0].id, { action: "Loading Summary" });
+
+            summarizeVideo(url);
+        });
+    };
+};
